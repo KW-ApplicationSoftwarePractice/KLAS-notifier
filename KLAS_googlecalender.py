526ff4a1149811e3554eb5efe61655d0d537d2c2
Polaris08 <tmdgus6237@naver.com>
2023-06-06 23:52:04 +0900

    Redesign klas_googlecalender.py
    Redesign klas_googlecalender.py

--- KLAS_googlecalender.py
+++ KLAS_googlecalender.py
@@ -10,13 +10,12 @@ def klas_automating(myID, myPW):
 
     flow = InstalledAppFlow.from_client_secrets_file(creds_filename, SCOPES)
     creds = flow.run_local_server(port=0)
-
     today = datetime.date.today().isoformat()
     service = build('calendar', 'v3', credentials=creds)
     
     event = {
             'summary': '', # 일정 제목
-            'description': '', # 일정 설명
+            'description': 'KLAS-notifier', # 일정 설명
             'start': { # 시작 날짜
                 'dateTime': today + 'T09:00:00',
                 'timeZone': 'Asia/Seoul'
@@ -27,6 +26,26 @@ def klas_automating(myID, myPW):
             }
         }
     
+    
+    #캘린더의 모든 일정 불러오기
+    page_token = None
+    event_list = []
+    
+    while True:
+        events_tmp = service.events().list(calendarId='primary', pageToken=page_token).execute()
+        for event_tmp in events_tmp['items']:
+            event_list.append(event_tmp)
+        page_token = events_tmp.get('nextPageToken')
+        if not page_token:
+            break
+
+    #Description 이 'KLAS-notifier' 이면 전부 삭제
+    for event_search in event_list:
+        if event_search.get("description") and event_search["description"] == "KLAS-notifier":
+            service.events().delete(calendarId='primary', eventId=event_search['id']).execute()
+        
+    
+    #KLAS의 미완료 항목과 동기화
     lecture_events = klas_notice.klas_notice_lecture(myID, myPW)
     for lecture_event in lecture_events:
         info_split = lecture_event.split('/')
@@ -37,7 +56,7 @@ def klas_automating(myID, myPW):
         event['start'] = {'dateTime' : time_text_list[0] + time_hour_and_min, 'timeZone' : 'Asia/Seoul'}
         event['end'] = event['start']
         service.events().insert(calendarId='primary', body=event).execute()
-    
+        
     homework_events = klas_notice.klas_notice_homework(myID, myPW)
     for homework_event in homework_events:
         info_split = homework_event.split('/')
