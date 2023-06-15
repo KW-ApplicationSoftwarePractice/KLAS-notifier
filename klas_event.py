b09ced22176a1693bb98212f8b08478a2f72c5d9
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2023-06-15 09:55:33 +0900

    klas_event 수정
    txt로 기존내역 저장, 업데이트 발생 시 확인

--- klas_event.py
+++ klas_event.py
@@ -8,6 +8,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 pages_list = [
     "https://klas.kw.ac.kr/std/lis/evltn/OnlineCntntsStdPage.do", #온라인 컨텐츠
     "https://klas.kw.ac.kr/std/lis/evltn/PrjctStdPage.do", #팀플
@@ -74,6 +77,22 @@ def accessOC(): #온라인 클래스에 접근
     # 내용의 업데이트 판별: OC_table을 txt 파일로 저장, 실행 시 마다 기존의 txt 파일과 새로 만들어진 OC_table 비교
     # 같지 않다면, 새로 만들어진 OC_table - txt 파일 의 내용을 전달해줌.
     # =============================================================================================
+    with open(os.path.join(BASE_DIR, 'latest_OC.txt'), 'r+') as f_read:
+        i=0
+        while(True):
+            if OC_table[i] != f_read.readline(i):
+                print("기존 캐시와 현재 강의게시판에 다른 내용이 있어요!")
+                isThereUpdate(True)
+                break
+            else:
+                i += 1
+        f_read.close()
+
+    with open(os.path.join(BASE_DIR, 'latest_OC.txt'), 'w+') as f_write:
+        for k in OC_table:
+            f_write.write(k+'\n')
+        f_write.close()
+
 def accessQZ():
     url = pages_list[6]
     driver.get(url)
@@ -113,6 +132,23 @@ def accessQZ():
             print("강의 없음")
     for k in QZ_table:
         print(k)
+    #Update된 내용, 캐시파일 저장 및 비교 및 업데이트
+    with open(os.path.join(BASE_DIR, 'latest_QZ.txt'), 'r+') as f_read:
+        i=0
+        while(True):
+            if QZ_table[i] != f_read.readline(i):
+                print("기존 캐시와 현재 퀴즈게시판에 다른 내용이 있어요!")
+                isThereUpdate(True)
+                break
+            else:
+                i += 1
+        f_read.close()
+
+    with open(os.path.join(BASE_DIR, 'latest_QZ.txt'), 'w+') as f_write:
+        for k in QZ_table:
+            f_write.write(k+'\n')
+        f_write.close()
+
 def accessHW():
     url = pages_list[5]
     driver.get(url)
@@ -149,13 +185,25 @@ def accessHW():
             print("과제 없음")
     for k in HW_table:
         print(k)
+    # 캐시
+    with open(os.path.join(BASE_DIR, 'latest_HW.txt'), 'r+') as f_read:
+        i=0
+        while(True):
+            if HW_table[i] != f_read.readline(i):
+                print("기존 캐시와 현재 과제게시판에 다른 내용이 있어요!")
+                isThereUpdate(True)
+                break
+            else:
+                i += 1
+        f_read.close()
 
-# update = tr[len(tr)-1].find_element(By.CLASS_NAME,"lft")
-# 가장 최근에 올라온 업데이트의 내용 텍스트 출력
-# print("update: ",update.text.strip())
-# with open(os.path.join(BASE_DIR, 'a.txt'), 'w+') as f:
-#     f.write(lists[0].text.strip())
-# for l in lists:
-#     #print(l.text.strip())
-#     x = l.text.strip().splitlines()
-#     print(x)
+    with open(os.path.join(BASE_DIR, 'latest_HW.txt'), 'w+') as f_write:
+        for k in HW_table:
+            f_write.write(k+'\n')
+        f_write.close()
+
+def isThereUpdate(bool):
+    if bool == True:
+        return 1;
+    else:
+        return 0;
