b18ec4df103571707bebf791275baeb9156abf6f
hyunnnchoi <37583279+hyunnnchoi@users.noreply.github.com>
2023-06-15 10:05:21 +0900

    Update klas_event.py
    headless

--- klas_event.py
+++ klas_event.py
@@ -22,7 +22,12 @@ pages_list = [
     "https://klas.kw.ac.kr/std/lis/sport/6972896bfe72408eb72926780e85d041/BoardListStdPage.do" #강의자료실
 ]
 
-driver = webdriver.Chrome('chromedriver.exe')  # 크롬드라이버 경로
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)  # 크롬드라이버 경로
 
 def klas_login(myID, myPW):
     url_login = "https://klas.kw.ac.kr/" #KLAS 로그인 페이지 주소
