b553fde1c67e0940619bef576c276b99f7112e97
shKim0316 <112600757+shKim0316@users.noreply.github.com>
2023-06-15 15:37:53 +0900

    C# WinForm
    test

---
+++ KLAS_app/KLAS_app/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Win_KLAS
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_btn = new System.Windows.Forms.Button();
+            this.ID_tb = new System.Windows.Forms.TextBox();
+            this.PW_tb = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(217, 127);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(91, 31);
+            this.login_btn.TabIndex = 0;
+            this.login_btn.Text = "로그인";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // ID_tb
+            // 
+            this.ID_tb.Location = new System.Drawing.Point(69, 26);
+            this.ID_tb.Name = "ID_tb";
+            this.ID_tb.Size = new System.Drawing.Size(239, 25);
+            this.ID_tb.TabIndex = 1;
+            // 
+            // PW_tb
+            // 
+            this.PW_tb.Location = new System.Drawing.Point(69, 77);
+            this.PW_tb.Name = "PW_tb";
+            this.PW_tb.PasswordChar = '*';
+            this.PW_tb.Size = new System.Drawing.Size(239, 25);
+            this.PW_tb.TabIndex = 2;
+            this.PW_tb.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PW_tb_KeyDown);
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(43, 29);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(20, 15);
+            this.ID.TabIndex = 3;
+            this.ID.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PW";
+            // 
+            // Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 180);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.PW_tb);
+            this.Controls.Add(this.ID_tb);
+            this.Controls.Add(this.login_btn);
+            this.Name = "Login_Form";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.TextBox ID_tb;
+        private System.Windows.Forms.TextBox PW_tb;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KLAS_app/KLAS_app/bin/Debug/KLAS_py/klas_renew.py
@@ -0,0 +1,266 @@
+import os
+import sys
+import time
+import datetime
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+def klas_renew(myID, myPW):
+    lecture_renew_class = []
+    lecture_renew_name = []
+    lecture_renew_time = []
+
+    homework_renew_class = []
+    homework_renew_name = []
+    homework_renew_time = []
+
+    quiz_renew_class = []
+    quiz_renew_name = []
+    quiz_renew_time = []
+
+    return_text = ""
+
+    driver = webdriver.Chrome('chromedriver.exe') #크롬드라이버 경로
+
+    url_login = "https://klas.kw.ac.kr/" #KLAS 로그인 페이지 주소
+    url_main = "https://klas.kw.ac.kr/std/cmn/frame/Frame.do" #메인 페이지 주소
+    url_lecture = "https://klas.kw.ac.kr/std/lis/evltn/LctrumHomeStdPage.do" #강의 주소
+    url_homework = "https://klas.kw.ac.kr/std/lis/evltn/TaskStdPage.do" #과제 주소
+    url_quiz = "https://klas.kw.ac.kr/std/lis/evltn/AnytmQuizStdPage.do" #퀴즈 주소
+
+
+    #KLAS 로그인
+    driver.get(url_login)
+    driver.find_element(By.ID, 'loginId').send_keys(myID)
+    driver.find_element(By.ID, 'loginPwd').send_keys(myPW)
+    driver.find_element(By.CLASS_NAME, 'btn').click()
+    time.sleep(3)
+
+
+    #강의 정보 불러오기
+    driver.get(url_lecture)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    time.sleep(3)
+
+    class_index = 1
+    try:
+        while(True):
+            driver.find_element(By.XPATH, '//*[@id="appSelectSubj"]/div[2]/div/div[2]/select/option[' + str(class_index) + ']').click()
+            driver.get(url_lecture)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            time.sleep(3)
+
+            lecture_tbody = soup.select('div.tablelistbox > table > tbody > tr')
+
+            if len(lecture_tbody) == 1:
+                pass
+            else:
+                for i in range(1,len(lecture_tbody)):
+                    if "100%" in lecture_tbody[i].select_one("td:nth-child(6)").text:
+                        pass
+                    else:
+                        lecture_renew_class.append(soup.select_one('#appHeaderSubj > div > div > div.col-md-10.subtitle > span.subjectname').text)
+                        lecture_renew_name.append((lecture_tbody[i].select_one("td:nth-child(4)")).get_text())
+                        lecture_renew_time.append((lecture_tbody[i].select_one("td:nth-child(5)")).get_text().strip())
+
+            class_index = class_index + 1
+            lecture_tbody.clear()
+    except:
+        pass
+
+
+    #과제 정보 불러오기
+    driver.get(url_homework)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    time.sleep(3)
+
+    class_index = 1
+    try:
+        while(True):
+            driver.find_element(By.XPATH, '//*[@id="appSelectSubj"]/div[2]/div/div[2]/select/option[' + str(class_index) + ']').click()
+            driver.get(url_homework)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            time.sleep(3)
+            
+            homework_table = soup.select('#appModule > div > div.tablelistbox > table > tbody')
+
+            if len(homework_table) == 1:
+                pass
+            else:
+                for i in range(0,len(homework_table)):
+                    if "미제출" in homework_table[i].select_one("tr:nth-child(1) > td:nth-child(4)").text:
+                        homework_renew_class.append(soup.select_one('#appHeaderSubj > div > div > div.col-md-10.subtitle > span.subjectname').text)
+                        homework_renew_name.append((homework_table[i].select_one("tr:nth-child(1) > td.lft")).get_text())
+                        homework_renew_time.append((homework_table[i].select_one("tr:nth-child(1) > td:nth-child(3)")).get_text().strip())
+                    else:
+                        pass
+
+            class_index = class_index + 1
+            homework_table.clear()
+    except:
+        pass
+
+
+    #퀴즈 정보 불러오기
+    driver.get(url_quiz)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    time.sleep(3)
+
+    class_index = 1
+    try:
+        while(True):
+            driver.find_element(By.XPATH, '//*[@id="appSelectSubj"]/div[2]/div/div[2]/select/option[' + str(class_index) + ']').click()
+            driver.get(url_quiz)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            time.sleep(3)
+
+            quiz_tbody = soup.select('#prjctList > tbody > tr')
+
+            if len(quiz_tbody) == 1:
+                pass
+            else:
+                for i in range(0,len(quiz_tbody)):
+                    if "미응시" in quiz_tbody[i].select_one("td:nth-child(6)").text:
+                        quiz_renew_class.append(soup.select_one('#appHeaderSubj > div > div > div.col-md-10.subtitle > span.subjectname').text)
+                        quiz_renew_name.append((quiz_tbody[i].select_one("td:nth-child(4)")).get_text())
+                        quiz_renew_time.append((quiz_tbody[i].select_one("td:nth-child(5)")).get_text().strip())
+                    else:
+                        pass
+
+            class_index = class_index + 1
+            quiz_tbody.clear()
+    except:
+        pass
+
+
+    #WinForm으로 전달할 return_text 구현
+    if (len(lecture_renew_class) != 0):
+        for i in range(0, len(lecture_renew_class)):
+            return_text = return_text + lecture_renew_class[i] + "////" + lecture_renew_name[i] + "////" + lecture_renew_time[i] + "\n"
+    else:
+        pass
+    return_text = return_text + "####" + "\n"
+    if (len(homework_renew_class) != 0):
+        for i in range(0, len(homework_renew_class)):
+            return_text = return_text + homework_renew_class[i] + "////" + homework_renew_name[i] + "////" + homework_renew_time[i] + "\n"
+    else:
+        pass
+    return_text = return_text + "####" + "\n"
+    if (len(quiz_renew_class) != 0):
+        for i in range(0, len(quiz_renew_class)):
+            return_text = return_text + quiz_renew_class[i] + "////" + quiz_renew_name[i] + "////" + quiz_renew_time[i] + "\n"
+    else:
+        pass
+
+    """
+    강의명1////강의이름1////강의기간1
+    강의명2////강의이름2////강의기간2
+    강의명3////강의이름3////강의기간3
+    ####
+    강의명1////과제이름1////과제기간1
+    강의명2////과제이름2////과제기간2
+    강의명3////과제이름3////과제기간3
+    ####
+    강의명1////퀴즈이름1////퀴즈기간1
+    강의명2////퀴즈이름2////퀴즈기간2
+    강의명3////퀴즈이름3////퀴즈기간3
+    """
+
+    print(return_text)
+
+
+    #KLAS와 Google Calender 동기화
+    creds_filename = 'credentials.json'
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    flow = InstalledAppFlow.from_client_secrets_file(creds_filename, SCOPES)
+    creds = flow.run_local_server(port=0)
+    today = datetime.date.today().isoformat()
+    service = build('calendar', 'v3', credentials=creds)
+
+    event = {
+                'summary': '', # 일정 제목
+                'description': 'KLAS-notifier', # 일정 설명
+                'start': { # 시작 날짜
+                    'dateTime': today + 'T09:00:00',
+                    'timeZone': 'Asia/Seoul'
+                },
+                'end': { # 종료 날짜
+                    'dateTime': today + 'T10:00:00',
+                    'timeZone': 'Asia/Seoul'
+                }
+            }
+        
+    #캘린더의 모든 일정 불러오기
+    page_token = None
+    event_list = []
+
+    while True:
+        events_tmp = service.events().list(calendarId='primary', pageToken=page_token).execute()
+        for event_tmp in events_tmp['items']:
+            event_list.append(event_tmp)
+        page_token = events_tmp.get('nextPageToken')
+        if not page_token:
+            break
+
+    #Description 이 'KLAS-notifier' 이면 전부 삭제
+    for event_search in event_list:
+        if event_search.get("description") and event_search["description"] == "KLAS-notifier":
+            service.events().delete(calendarId='primary', eventId=event_search['id']).execute()
+
+    #KLAS의 미완료 항목과 동기화
+    info_split = []
+    if (len(lecture_renew_class) != 0): #강의 동기화
+        for i in range (0, len(lecture_renew_class)):
+            info_split = [lecture_renew_class[i], lecture_renew_name[i], lecture_renew_time[i]]
+            time_text = (info_split[2])[-16:] #마감시간만 가져옴 (2023-06-11 23:59)
+            time_text_list = time_text.split(' ') #time_text_list[0] = "2023-06-11", time_text_list[1] = "23:59"
+            time_hour_and_min = 'T' + time_text_list[1] + ':00' 
+            event['summary'] = "[" + info_split[0] + "] " + info_split[1]
+            event['start'] = {'dateTime' : time_text_list[0] + time_hour_and_min, 'timeZone' : 'Asia/Seoul'}
+            event['end'] = event['start']
+            service.events().insert(calendarId='primary', body=event).execute()
+    else:
+        pass
+    if (len(homework_renew_class) != 0): #과제 동기화
+        for i in range (0, len(homework_renew_class)):
+            info_split = [homework_renew_class[i], homework_renew_name[i], homework_renew_time[i]]
+            time_text = (info_split[2])[-19:] #마감시간만 가져옴 (2023-06-11 23:59:59)
+            time_text_list = time_text.split(' ') #time_text_list[0] = "2023-06-11", time_text_list[1] = "23:59:59"
+            time_hour_and_min = 'T' + time_text_list[1]
+            event['summary'] = "[" + info_split[0] + "] " + info_split[1]
+            event['start'] = {'dateTime' : time_text_list[0] + time_hour_and_min, 'timeZone' : 'Asia/Seoul'}
+            event['end'] = event['start']
+            service.events().insert(calendarId='primary', body=event).execute()
+    else:
+        pass
+    if (len(quiz_renew_class) != 0): #퀴즈 동기화
+        for i in range (0, len(quiz_renew_class)):
+            info_split = [quiz_renew_class[i], quiz_renew_name[i], quiz_renew_time[i]]
+            time_text = (info_split[2])[-16:] #마감시간만 가져옴 (2023-06-11 23:59)
+            time_text_list = time_text.split(' ') #time_text_list[0] = "2023-06-11", time_text_list[1] = "23:59"
+            time_hour_and_min = 'T' + time_text_list[1] + ':00'
+            event['summary'] = "[" + info_split[0] + "] " + info_split[1]
+            event['start'] = {'dateTime' : time_text_list[0] + time_hour_and_min, 'timeZone' : 'Asia/Seoul'}
+            event['end'] = event['start']
+            service.events().insert(calendarId='primary', body=event).execute()
+    else:
+        pass
+
+klas_renew(sys.argv[1], sys.argv[2])
